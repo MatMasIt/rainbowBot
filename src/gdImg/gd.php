3bd3923712028f9cf0439a25c52ec64d63c28c76
MatMasIt <mattia.mascarello@outlook.it>
2021-08-24 03:18:02 +0200

    Fixed demigirl, added mlm card

--- src/gdImg/gd.php
+++ src/gdImg/gd.php
@@ -16,7 +16,8 @@ function flagGetFromUser($p){
 	if (contains("ETERO", $o) || contains("HET", $o)) $re[]= "etero";
   //orientation, romantic
   if (contains("BIROM", $o)) $re[]= "Biromantic";
- if (contains("DEMIG", $o)) $re[]= "Biromantic";
+ if (contains("DEMIG", $o)) $re[]= "demigirl";
+  if (contains("MLM", $o)) $re[]= "mlm";
 	//gender-related
 		if(contains("MTF",$o) || contains("FTM",$o)  || contains("TRANS",$o)) $re[]="trans";
 		return $re;
